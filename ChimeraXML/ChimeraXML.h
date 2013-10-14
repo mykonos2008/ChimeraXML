f909774d91574535bf89a14d1a91649b8304111f
mykonos <mykonos2008@hotmail.co.jp>
2013-10-14 21:46:59 +0900

    Initial Commit

---
+++ ChimeraXML/ChimeraXML.h
@@ -0,0 +1,13 @@
+//
+//  ChimeraXML.h
+//  ChimeraXML
+//
+//  Created by mykonos on 13/10/14.
+//  Copyright (c) 2013年 mykonos2008. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChimeraXML : NSObject
+
+@end
---
+++ ChimeraXML/ChimeraXML.m
@@ -0,0 +1,13 @@
+//
+//  ChimeraXML.m
+//  ChimeraXML
+//
+//  Created by mykonos on 13/10/14.
+//  Copyright (c) 2013年 mykonos2008. All rights reserved.
+//
+
+#import "ChimeraXML.h"
+
+@implementation ChimeraXML
+
+@end
